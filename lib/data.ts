9766cbc55140e315760d2e1bef61cf7f8b9d2054
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-21 11:58:23 -0300

    content: experienceData

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,13 @@
+import React from 'react';
 
+// **************************** Icons
+import {
+  FaChalkboardTeacher,
+  FaReact,
+} from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+
+// **************************** Content
 export const siteMetadata = {
   title: 'Matías | Portfolio Personal',
   description: 'Matías es un desarrollador con 2 años de experiencia en desarrollo frontend',
@@ -31,6 +40,49 @@ export const navLinks = [
   {
     name: "Contacto",
     hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Soporte Técnico y Sysadmin",
+    company: "Generación WiFi",
+    location: "Argentina",
+    description: "Responsable de servicio técnico y sysadmin (Windows Server y Linux).",
+    date: "2004 - 2017",
+    icon: React.createElement(LuGraduationCap),
+  },
+  {
+    title: "Traductor de Inglés",
+    company: "Freelance",
+    location: "Argentina",
+    description: "Traducción técnica y legal.",
+    date: "2015 - 2023",
+    icon: React.createElement(FaReact),
+  },
+  {
+    title: "Aprendizaje autodidacta",
+    company: "",
+    location: "Argentina",
+    description: "Aprendizaje autodidacta de tecnologías frontend y backend.",
+    date: "2021 - 2022",
+    icon: React.createElement(LuGraduationCap),
+  },
+  {
+    title: "Desarrollador Frontend Freelance",
+    company: "Freelance",
+    location: "Argentina",
+    description: "Desarrollo de aplicaciones web utilizando React, Next.js y Tailwind CSS.",
+    date: "2022 - 2024",
+    icon: React.createElement(FaReact),
+  },
+  {
+    title: "Technical Content Creator",
+    company: "DonWeb",
+    location: "Argentina",
+    description: "Creación de contenido técnico y talleres sobre devops, CI/CD, Docker, Linux, Frontend, Backend, etc.",
+    date: "2024 - actualidad",
+    icon: React.createElement(FaChalkboardTeacher),
   },
 ] as const;
 
