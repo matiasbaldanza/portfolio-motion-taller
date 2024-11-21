820cb981ed460d2663bd986aead789157e2d8738
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-21 20:42:42 -0300

    fix: update SectionContainer to allow HTMLElement props (className)

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import SectionHeading from '@/components/SectionHeading'
 import { navLinks } from '@/lib/data'
+import clsx from 'clsx'
+
 
 // Remove the '#' from the href to create the section id
 // This assumes these ids are unique and the hrefs in 
 // the Nav are all anchor links that start with '#'
 type RemoveHash<T extends string> = T extends `#${infer Rest}` ? Rest : T;
 
-interface SectionContainerProps {
+interface SectionContainerProps
+  extends React.HTMLAttributes<HTMLElement> {
   children?: React.ReactNode
   heading: string
   id: RemoveHash<typeof navLinks[number]['href']>
@@ -16,14 +19,18 @@ interface SectionContainerProps {
 export default function SectionContainer({
   children,
   heading,
-  id
+  id,
+  className
 }: SectionContainerProps) {
   console.log(id)
 
   return (
     <section
       id={id}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className={clsx(
+        'mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28',
+        className
+      )}
     >
       <SectionHeading>
         {heading}
