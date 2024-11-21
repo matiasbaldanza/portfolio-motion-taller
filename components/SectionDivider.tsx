0f949d40473462daf501f8ff31cb8315d4518df0
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-21 14:10:19 -0300

    feat: add SectionDivider (spacer, will be animated)

---
+++ components/SectionDivider.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SectionDivider() {
+  return (
+    <div
+      className="hidden w-1 h-16 my-24 bg-gray-200 rounded-full sm:block dark:bg-opacity-20"
+    ></div>
+  );
+}
