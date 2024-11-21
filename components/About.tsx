d124c7b6b4893b86fb2c427c43fb998e6d461920
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-21 19:31:33 -0300

    feat: add section placeholders and initial About section (no content)

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeading from './SectionHeading'
+
+export default function About() {
+  return (
+    <section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+    >
+      <SectionHeading>
+        Acerca de mí
+      </SectionHeading>
+    </section>
+  )
+}
