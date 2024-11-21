a141f9776db73ae27822f7c2aa926fa1386aba61
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-21 12:51:33 -0300

    style: add styling to main container, change document language to 'es'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { siteMetadata } from '@/lib/data'
-
 const inter = Inter({ subsets: ['latin'] })
+
+import Header from '@/components/Header'
 
 export const metadata = {
   title: siteMetadata.title,
@@ -15,12 +16,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="es" className="!scroll-smooth">
+      <body className={
+        `${inter.className} 
+        bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90
+        
+        `}>
+
         {/* Gradient background */}
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
 
+        <Header />
         {children}
       </body>
     </html>
