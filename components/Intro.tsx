5b9ecff71a05f4bed8e3d736a0cad709c6d80f1f
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-21 23:39:45 -0300

    fix: adjust action buttons colors in dark mode

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -45,7 +45,7 @@ export default function Intro() {
       </h1>
 
       {/* Employment links */}
-      <div className="flex flex-col items-center justify-center gap-2 text-sm text-gray-800 sm:flex-row [&>*]:w-fit">
+      <div className="flex flex-col items-center justify-center gap-4 sm:gap-2 text-sm text-gray-800 sm:flex-row [&>*]:w-fit">
         <Link href="#"
           className="flex items-center gap-2 py-3 text-white transition bg-gray-900 rounded-full outline-none group px-7 focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-95"
         >
@@ -55,7 +55,7 @@ export default function Intro() {
 
         <Link
           href="#"
-          className="flex items-center gap-2 py-3 transition bg-white rounded-full outline-none cursor-pointer group px-7 focus:scale-105 hover:scale-105 active:scale-95 borderBlack dark:bg-white/10"
+          className="flex items-center gap-2 py-3 transition bg-white rounded-full outline-none cursor-pointer group px-7 focus:scale-105 hover:scale-105 active:scale-95 borderBlack dark:bg-white/30 dark:text-white/90"
           download
         >
           Descargar CV {" "}
@@ -65,10 +65,10 @@ export default function Intro() {
         {/* Social links displayed in a single row */}
         <div className="flex items-center gap-2">
           <Link href={linkedin}
-            className="bg-white p-3 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-3 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/90"
           ><FaLinkedin size="1.8em" className="text-[#0077b5]" /></Link>
           <Link href={github}
-            className="bg-white p-3 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-3 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/90"
           ><FaGithubSquare size="1.8em" className="text-[#6e5494]" /></Link>
         </div>
       </div>
