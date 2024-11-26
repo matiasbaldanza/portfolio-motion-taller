9ac239cac473d52006b93a267cc9d7aa18176e98
Matias Baldanza <matiasbaldanza@gmail.com>
2024-11-26 16:57:23 -0300

    animate Skills section using staggered animation

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,20 @@
+'use client'
+
 import React from 'react'
 import SectionContainer from '@/components/SectionContainer'
 import { skillsData } from '@/lib/data'
+import { motion } from 'framer-motion'
+
+
+const fadeInVariants = {
+  initial: { opacity: 0, y: 100 },
+  final: (index: number) => ({
+    opacity: 1, y: 0,
+    transition: {
+      delay: 0.05 * index
+    }
+  })
+}
 
 export default function Skills() {
   return (
@@ -11,11 +25,18 @@ export default function Skills() {
     >
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li key={index}
+          <motion.li key={index}
             className="px-5 py-3 bg-white shadow-sm borderBlack rounded-xl dark:bg-white/10 dark:text-white/80"
+            variants={fadeInVariants}
+            initial="initial"
+            whileInView="final"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </SectionContainer>
